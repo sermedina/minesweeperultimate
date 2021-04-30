c88c20f4f631a382f18ad5cbf5cb41ee5d95b939
Sergio Medina <sergio.medina@tuten.cl>
2021-04-30 12:57:35 -0400

    Procfile test

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 --java -Dserver.port= $PORT -jar target/*.jar
-java -jar $JAVA_OPTS -Dserver.port=$PORT target/*.jar
+web : java -jar $JAVA_OPTS -Dserver.port=$PORT target/*.jar
--- src/main/java/com/deviget/minesweeperultimate/Main.java
+++ src/main/java/com/deviget/minesweeperultimate/Main.java
@@ -6,12 +6,14 @@ package com.deviget.minesweeperultimate;
  */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
+
 
 @SpringBootApplication
 @ImportResource({"classpath:appconfig-mvc.xml","classpath:appconfig-data.xml"})
 
-public class Main {
+public class Main extends SpringBootServletInitializer {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Main.class, args);
