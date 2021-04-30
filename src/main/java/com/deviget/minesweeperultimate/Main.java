1fd30db5d63afde939a5d22c2fc7bbc356b2cf5b
Sergio Medina <sergio.medina@tuten.cl>
2021-04-30 13:14:01 -0400

    Procfile test 2

--- src/main/java/com/deviget/minesweeperultimate/Main.java
+++ src/main/java/com/deviget/minesweeperultimate/Main.java
@@ -8,12 +8,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-
+@Controller
 @SpringBootApplication
-@ImportResource({"classpath:appconfig-mvc.xml","classpath:appconfig-data.xml"})
+//@ImportResource({"classpath:appconfig-mvc.xml","classpath:appconfig-data.xml"})
 
 public class Main extends SpringBootServletInitializer {
+    
+        @RequestMapping(value ="/", method = RequestMethod.GET)
+        public String mainPage() {
+            return "home";
+        }
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Main.class, args);
